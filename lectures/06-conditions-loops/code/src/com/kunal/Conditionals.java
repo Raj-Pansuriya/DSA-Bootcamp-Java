c48b78ebda4d3a4df7f0821a81056f434a11aa1b
Raj-Pansuriya <72313592+Raj-Pansuriya@users.noreply.github.com>
2021-11-17 16:51:22 +0530

    Fix a logical error
    The conditions should be either in Ascending or descending order when writing `if-else-if` so no condition is skipped

--- lectures/06-conditions-loops/code/src/com/kunal/Conditionals.java
+++ lectures/06-conditions-loops/code/src/com/kunal/Conditionals.java
@@ -21,9 +21,9 @@ public class Conditionals {
 
         // multiple if-else
 
-//        if (salary > 10000) {
+//        if (salary > 20000) {
 //            salary += 2000; // salary = salary + 2000
-//        } else if (salary > 20000) {
+//        } else if (salary > 10000) {
 //            salary += 3000;
 //        } else {
 //            salary += 1000;
